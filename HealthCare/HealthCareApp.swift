63c23308bdc38cf74cf895f267681aafbe214ab9
crydder7 <getright1337ez@gmail.com>
2025-11-26 01:33:11 +0300

    Initial Commit

---
+++ HealthCare/HealthCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCareApp.swift
+//  HealthCare
+//
+//  Created by lonely. on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
